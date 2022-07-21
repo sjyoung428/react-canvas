b06ac4a30433665a5d05428ae632bed2703532ca
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-07-21 20:00:13 +0900

    Feat: canvas에 그림그리기

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -1,18 +1,43 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { MouseEventHandler, useEffect, useRef, useState } from "react";
 import { S } from "./styled";
 
 const Canvas = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [context, setContext] = useState<CanvasRenderingContext2D | null>();
+  const [isPainting, setIsPainting] = useState(false);
+
   useEffect(() => {
     if (canvasRef.current) {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext("2d");
       setContext(ctx);
-      context?.strokeRect(50, 50, 100, 200); // x좌표, y좌표, 가로, 세로
-    }
-  }, []);
+      canvas.addEventListener("mousemove", onMove);
+      canvas.addEventListener("mousedown", startPainting);
+      canvas.addEventListener("mouseup", cancelPainting);
+      canvas.addEventListener("mouseleave", cancelPainting);
 
+      return () => {
+        canvas.removeEventListener("mousemove", onMove);
+        canvas.removeEventListener("mousedown", startPainting);
+        canvas.removeEventListener("mouseup", cancelPainting);
+        canvas.removeEventListener("mouseleave", cancelPainting);
+      };
+    }
+  }, [context, isPainting]);
+
+  const onMove = (event: MouseEvent) => {
+    if (isPainting) {
+      context?.lineTo(event.offsetX, event.offsetY);
+      context?.stroke();
+    }
+    context?.moveTo(event.offsetX, event.offsetY);
+  };
+  const startPainting = () => {
+    setIsPainting(true);
+  };
+  const cancelPainting = () => {
+    setIsPainting(false);
+  };
   return <S.Canvas height={500} width={500} ref={canvasRef} />;
 };
 
